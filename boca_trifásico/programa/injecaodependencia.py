baaa74e0cddcd127e3230e5a1dcfc5842b7ba6bf
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2024-08-30 17:05:30 -0300

    deleted:    "boca_trif\303\241sico/programa/__pycache__/injecaodependencia.cpython-312.pyc" 	modified:   "boca_trif\303\241sico/programa/injecaodependencia.py"

--- boca_trifásico/programa/injecaodependencia.py
+++ boca_trifásico/programa/injecaodependencia.py
@@ -20,28 +20,30 @@ def verify_all_dependencies(dictionary:dict):
     for keys in dictionary:
         if dictionary[keys] == tuple():
             return True
-    print("usar injecao tardia")
-    exit()
+    # print("usar injecao tardia")
+    return "usar injecao tardia"
     
 def main_verification(dictionary:dict):
-    
+
     for keys in dictionary:
+ 
         for values in dictionary[keys]:
-        
+           
             value_key_index = 0
             
             while True:
 
                 try:
                     if dictionary[values][value_key_index] == keys:
-                        print('usar injecao tardia')
-                        exit()
+                        # print('usar injecao tardia')
+                        return 'usar injecao tardia'
 
                 except IndexError:
                     break
                 value_key_index+=1
             
-    print('ok')
+    # print('ok')
+    return 'ok'
 dictonary_master = dict()
 loops = int(input())
 letters = []
@@ -55,9 +57,9 @@ for i in range(loops):
     except KeyError:
         dictonary_master[dependencies[0]] = (dependencies[1])
 increase_empty_values(dictonary_master,letters)
-verify_all_dependencies(dictonary_master)
 
+if verify_all_dependencies(dictonary_master) != True:
+    print(verify_all_dependencies(dictonary_master))
+else:
+    print(main_verification(dictonary_master))
 
-
-
-main_verification(dictonary_master)
