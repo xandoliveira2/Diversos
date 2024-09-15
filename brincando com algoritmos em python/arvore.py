68cd57defd5fba390a3af405c0253a2c2aa75b17
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2024-09-15 14:29:17 -0300

    parte teorica de busca-binaria/ próximo será criar a organização e o metodo de alinhamento

--- brincando com algoritmos em python/arvore.py
+++ brincando com algoritmos em python/arvore.py
@@ -1,8 +1,9 @@
 
 arvore_geral = [
-    [1],
-[2,3],
-[4,5,6,7]
+    [19],
+[13,17],
+[7,11,3,7],
+[5]
 
 ]
 potencia = 0
@@ -14,17 +15,34 @@ def take_left_side():
     for i in range(1,len(arvore_geral)):
         retornar.append(arvore_geral[i][0:2**pot])
         pot+=1
-
-    print(retornar)
+    return retornar
 def take_right_side():
     retornar = [arvore_geral[0][0]]
     pot = 0
     for i in range(1,len(arvore_geral)):
         retornar.append(arvore_geral[i][2**pot:])
         pot+=1
+    return retornar
 
-    print(retornar)
-
-
-take_left_side()
-take_right_side()
+def formatar(particao):
+    retornar = []
+    for i in particao:
+        if type(i) == list:
+            for j in i:
+                retornar.append(j)
+        else:
+            retornar.append(i)
+    return retornar
+l = take_left_side()
+r = take_right_side()
+l = formatar(l)
+r = formatar(r)
+print(r,'  ',list(reversed(r)))
+print(l)
+if l == sorted(l) and r == sorted(r):
+    print(1)
+elif l == list(reversed(sorted(l))) and r == list(reversed(sorted(r))): # tudo errado, preciso criar uma função que retorne ela organizada e desorganizada corretamente
+    #o que diabos passou na cabeça p botar essa merda?
+    print(2)
+else:
+    print(0)
