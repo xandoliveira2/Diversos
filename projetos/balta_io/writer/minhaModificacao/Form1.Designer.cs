dd687f701078fb8b92c7315b0e0efe4e8599ec12
Alexandre Alcindo <142458591+xandoliveira2@users.noreply.github.com>
2025-04-24 17:32:23 -0300

    Add files via upload

---
+++ projetos/balta_io/writer/minhaModificacao/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TextEditorWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnEscrever = new System.Windows.Forms.Button();
+            this.txtTextoGravar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNomeArquivo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(23, 25);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir Arquivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnEscrever
+            // 
+            this.btnEscrever.Location = new System.Drawing.Point(104, 25);
+            this.btnEscrever.Name = "btnEscrever";
+            this.btnEscrever.Size = new System.Drawing.Size(96, 23);
+            this.btnEscrever.TabIndex = 1;
+            this.btnEscrever.Text = "Escrever Arquivo";
+            this.btnEscrever.UseVisualStyleBackColor = true;
+            this.btnEscrever.Click += new System.EventHandler(this.btnEscrever_Click);
+            // 
+            // txtTextoGravar
+            // 
+            this.txtTextoGravar.Location = new System.Drawing.Point(23, 79);
+            this.txtTextoGravar.Multiline = true;
+            this.txtTextoGravar.Name = "txtTextoGravar";
+            this.txtTextoGravar.Size = new System.Drawing.Size(357, 121);
+            this.txtTextoGravar.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite o conteúdo do arquivo";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Digite o nome do arquivo";
+            // 
+            // txtNomeArquivo
+            // 
+            this.txtNomeArquivo.Location = new System.Drawing.Point(221, 27);
+            this.txtNomeArquivo.Name = "txtNomeArquivo";
+            this.txtNomeArquivo.Size = new System.Drawing.Size(122, 20);
+            this.txtNomeArquivo.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 227);
+            this.Controls.Add(this.txtNomeArquivo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTextoGravar);
+            this.Controls.Add(this.btnEscrever);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnEscrever;
+        private System.Windows.Forms.TextBox txtTextoGravar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txtNomeArquivo;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ projetos/balta_io/writer/minhaModificacao/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextEditorWindowsForms
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string texto = "";
+                using (OpenFileDialog file = new OpenFileDialog())
+                {
+                    file.Title = "Selecione um arquivo";
+                    file.Filter = "Arquivos de texto (*.txt)|*.txt";
+                    if (file.ShowDialog() == DialogResult.OK)
+                    {
+                        using (var reader = new StreamReader(file.FileName))
+                        {
+                            texto = reader.ReadToEnd();
+                            MessageBox.Show(texto,"Conteúdo do arquivo");
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Algum erro ocorreu durante a execução do programa : {ex.Message}", "Erro", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnEscrever_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                errorProvider1.SetError(txtTextoGravar, string.Empty);
+                errorProvider1.SetError(txtNomeArquivo, string.Empty);
+                if(string.IsNullOrWhiteSpace(txtTextoGravar.Text))
+                {
+                    errorProvider1.SetError(btnEscrever, "Para escrever em um arquivo precisa ter um texto válido");
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(txtNomeArquivo.Text))
+                {
+                    errorProvider1.SetError(btnEscrever, "Para escrever em um arquivo precisa ter um nome de arquivo válido");
+                    return;
+                }
+                errorProvider1.SetError(txtTextoGravar, string.Empty);
+                errorProvider1.SetError(txtNomeArquivo, string.Empty);
+                string caminhoPasta = "";
+                using(FolderBrowserDialog browser = new FolderBrowserDialog())
+                {
+                    browser.Description = "Selecione uma pasta para salvar o arquivo";
+                    if (browser.ShowDialog() == DialogResult.OK)
+                    {
+                        caminhoPasta = browser.SelectedPath;
+                    }
+                }
+
+                using(StreamWriter writer = new StreamWriter($"{caminhoPasta}\\{txtNomeArquivo.Text}.txt"))
+                {
+                    writer.Write(txtTextoGravar.Text);
+                }
+
+                MessageBox.Show($"Arquivo salvo com sucesso em : {caminhoPasta}\\{txtNomeArquivo.Text}.txt","Gravado com sucesso",MessageBoxButtons.OKCancel);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Algum erro ocorreu durante a execução do programa : {ex.Message}", "Erro", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                txtTextoGravar.Clear();
+                txtNomeArquivo.Clear();
+            }
+        }
+    }
+}
