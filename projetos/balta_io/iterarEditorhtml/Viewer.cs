3ebcb9e2c4938bf71ac044e9e5de621363b23e3a
Alexandre Alcindo <142458591+xandoliveira2@users.noreply.github.com>
2025-04-28 17:05:20 -0300

    Add files via upload

---
+++ projetos/balta_io/iterarEditorhtml/Viewer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace EditorHtml
+{
+    public class Viewer
+    {
+
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("-----------------");
+            Replace(text);
+            Console.WriteLine("-----------------");
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            // strong.IsMatch
+        }
+    }
+
+}
