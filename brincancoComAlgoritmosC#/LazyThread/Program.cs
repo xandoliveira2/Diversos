4af535efdbdc312a9ab1a20d2bae6dc114b24c8c
Alexandre Oliveira <alexandrealcindooliveira@hotmail.com>
2025-05-22 13:38:46 -0300

    datetime python

---
+++ brincancoComAlgoritmosC#/LazyThread/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+// namespace LazyThreadEx1
+// {
+//     public class Program
+//     {
+
+//         static void Main(string[] args)
+//         {
+//             Lazy<Relatorio> relatorio = new Lazy<Relatorio>(() => new Relatorio(), LazyThreadSafetyMode.ExecutionAndPublication);
+//             relatorio.Value.Exibir();
+//         }
+
+//         public class Relatorio
+//         {
+//             public Relatorio()
+//             {
+//                 Console.WriteLine("Relatório foi carregado.");
+//             }
+
+//             public void Exibir()
+//             {
+//                 Console.WriteLine("Exibindo relatório...");
+//             }
+//         }
+
+//     }
+// }
+
+
+
+
+namespace LazyThreadEx2
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Lazy<UsuarioLogado> user = new Lazy<UsuarioLogado>(() => new UsuarioLogado(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+            // user.Value.ObterNome();
+        }
+
+
+        public class UsuarioLogado
+        {
+            // private string Nome {
+            //     get
+            //     { return Nome; }
+            //     set
+            //     { (string nome) => Nome = nome; } }
+
+            private string Nome = "Nome qualquer";
+            public UsuarioLogado()
+            {
+                Console.WriteLine("Buscando dados do usuário logado...");
+            }
+
+            public void ObterNome() => Console.WriteLine(Nome);            
+
+
+        }
+
+
+    }
+}
---
+++ brincancoComAlgoritmosC#/LazyThread/teste.py
@@ -0,0 +1,9 @@
+import datetime
+
+a = datetime.datetime.now()
+a += datetime.timedelta(days=10)
+#timedelta = espaço de tempo
+print(a)
+print(type(a))
+b = datetime.datetime(2025,9,30)
+print(b)
